6c64de48566fe7c8d0006e36e28a4b70afbb03a0
Peniaz Evgenii <evgenii.peniaz@gmail.com>
2024-10-31 16:19:26 +0300

    correct application.yaml and start work with liquibase

---
+++ user-service/src/main/resources/db/changelog/db.changelog-1.0.sql
@@ -0,0 +1,41 @@
+--liquibase formatted sql
+
+--changeset Evgenii:1
+create SCHEMA IF NOT EXISTS app
+    AUTHORIZATION postgres;
+
+CREATE TABLE IF NOT EXISTS app.users
+(
+    id uuid NOT NULL,
+    dt_create timestamp without time zone NOT NULL,
+    dt_update timestamp without time zone NOT NULL,
+    mail character varying NOT NULL,
+    password character varying NOT NULL,
+    fio character varying NOT NULL,
+    role character varying NOT NULL,
+    status character varying NOT NULL,
+    create_by character varying,
+    update_by character varying,
+    CONSTRAINT users_pkey PRIMARY KEY (id),
+    CONSTRAINT mail_unique UNIQUE (mail)
+);
+
+ALTER TABLE IF EXISTS app.users
+    OWNER to postgres;
+
+--changeset Evgenii:2
+CREATE TABLE app.codes
+(
+    id bigserial,
+    user_id uuid NOT NULL,
+    code character varying NOT NULL,
+    PRIMARY KEY (id),
+    CONSTRAINT fk_user_id FOREIGN KEY (user_id)
+        REFERENCES public.users (id) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION
+        NOT VALID
+);
+
+ALTER TABLE IF EXISTS app.codes
+    OWNER to postgres;
