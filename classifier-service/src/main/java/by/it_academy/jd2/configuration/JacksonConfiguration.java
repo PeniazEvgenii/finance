d2f98660e0a3f2b7a557204253ffa639e4e4a74d
Peniaz Evgenii <evgenii.peniaz@gmail.com>
2024-11-01 13:33:06 +0300

    work with configuration, properties, pom

---
+++ classifier-service/src/main/java/by/it_academy/jd2/configuration/JacksonConfiguration.java
@@ -0,0 +1,20 @@
+package by.it_academy.jd2.configuration;
+
+import by.it_academy.jd2.jackson.InstantToMicrosecondsSerializer;
+import by.it_academy.jd2.jackson.MicrosecondsToInstantConverter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class JacksonConfiguration {
+
+    @Bean
+    public MicrosecondsToInstantConverter microsecondsToInstantConverter() {
+        return new MicrosecondsToInstantConverter();
+    }
+
+    @Bean
+    public InstantToMicrosecondsSerializer instantToMicrosecondsSerializer() {
+        return new InstantToMicrosecondsSerializer();
+    }
+}
---
+++ classifier-service/src/main/resources/db/changelog/db.changelog-1.0.sql
@@ -0,0 +1,33 @@
+--liquibase formatted sql
+
+--changeset Evgenii:1
+create SCHEMA IF NOT EXISTS classifier
+    AUTHORIZATION postgres;
+
+CREATE TABLE IF NOT EXISTS classifier.currencies
+(
+    id uuid,
+    title character varying NOT NULL,
+    description character varying NOT NULL,
+    dt_create timestamp without time zone NOT NULL,
+    dt_update timestamp without time zone NOT NULL,
+    CONSTRAINT currencies_pkey PRIMARY KEY (id),
+    CONSTRAINT title_currency_unique UNIQUE (title)
+);
+
+ALTER TABLE IF EXISTS classifier.currencies
+    OWNER to postgres;
+
+--changeset Evgenii:2
+CREATE TABLE classifier.operation_categories
+(
+    id uuid,
+    title character varying NOT NULL,
+    dt_create timestamp without time zone NOT NULL,
+    dt_update timestamp without time zone NOT NULL,
+    CONSTRAINT currencies_pkey PRIMARY KEY (id),
+    CONSTRAINT title_category_unique UNIQUE (title)
+);
+
+ALTER TABLE IF EXISTS classifier.operation_categories
+    OWNER to postgres;
