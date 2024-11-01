15e79f6d4908fc7c641d5eb4a4ae4c678f9b09f5
Peniaz Evgenii <evgenii.peniaz@gmail.com>
2024-11-01 13:28:33 +0300

    create controller for classifier-service

---
+++ classifier-service/src/main/java/by/it_academy/jd2/controller/ClassifierController.java
@@ -0,0 +1,47 @@
+package by.it_academy.jd2.controller;
+
+import by.it_academy.jd2.page.PageOf;
+import by.it_academy.jd2.service.IClassifierService;
+import by.it_academy.jd2.service.dto.*;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api/v1/classifier")
+public class ClassifierController {
+
+    private final IClassifierService classifierService;
+
+    @PostMapping("/currency")
+    @ResponseStatus(value = HttpStatus.CREATED)
+    public void createCurrency(@RequestBody @Valid CurrencyCreateDto currencyCreateDto) {
+
+        classifierService.createCurrency(currencyCreateDto);
+    }
+
+    @GetMapping("/currency")
+    @ResponseStatus(value = HttpStatus.OK)
+    public PageOf<CurrencyReadDto> findAllCurrency(@RequestParam(value = "page", defaultValue = "0") Integer page,
+                                                   @RequestParam(value = "size", defaultValue = "20") Integer size) {
+
+        return classifierService.findAllCurrency(new PageDto(page, size));
+    }
+
+    @PostMapping("/operation/category")
+    @ResponseStatus(value = HttpStatus.CREATED)                                                                         //опред может валид все на сервис
+    public void createOperationCategory(@RequestBody @Valid OperationCategoryCreateDto operationCategoryCreateDto) {
+
+        classifierService.createOperationCategory(operationCategoryCreateDto);
+    }
+
+    @GetMapping("/operation/category")
+    @ResponseStatus(value = HttpStatus.OK)
+    public PageOf<OperationCategoryReadDto> findAllOperationCategory(@RequestParam(value = "page", defaultValue = "0") Integer page,
+                                                                     @RequestParam(value = "size", defaultValue = "20") Integer size) {
+
+        return classifierService.findAllOperationCategory(new PageDto(page, size));
+    }
+}
---
+++ classifier-service/src/main/java/by/it_academy/jd2/controller/handler/ControllerExceptionHandler.java
@@ -0,0 +1,80 @@
+package by.it_academy.jd2.controller.handler;
+
+import by.it_academy.jd2.error.EError;
+import by.it_academy.jd2.error.ErrorResponse;
+import by.it_academy.jd2.error.StructuredError;
+import by.it_academy.jd2.error.StructuredErrorResponse;
+import jakarta.validation.ConstraintViolationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.NoHandlerFoundException;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+import org.springframework.web.servlet.resource.NoResourceFoundException;
+
+import java.util.List;
+
+@RestControllerAdvice
+public class ControllerExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<StructuredErrorResponse> onMethodArgumentNotValidException(
+            MethodArgumentNotValidException exception) {
+
+        List<StructuredError> errors = exception.getBindingResult()
+                .getFieldErrors()
+                .stream()
+                .map(error -> new StructuredError(error.getDefaultMessage(), error.getField()))
+                .toList();
+        StructuredErrorResponse structuredErrorResponse = new StructuredErrorResponse(EError.STRUCTURED_ERROR, errors);
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body(structuredErrorResponse);
+    }
+
+    @ExceptionHandler(ConstraintViolationException.class)
+    public ResponseEntity<StructuredErrorResponse> onConstraintValidationException(
+            ConstraintViolationException exception) {
+
+        List<StructuredError> errors = exception.getConstraintViolations()
+                .stream()
+                .map(violation -> {
+                    String fullPath = violation.getPropertyPath().toString();
+                    String fieldName = fullPath.contains(".")
+                            ? fullPath.substring(fullPath.lastIndexOf(".") + 1)
+                            : fullPath;
+
+                    return new StructuredError(violation.getMessage(), fieldName);
+                })
+                .toList();
+        StructuredErrorResponse structuredErrorResponse = new StructuredErrorResponse(EError.STRUCTURED_ERROR, errors);
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body(structuredErrorResponse);
+    }
+
+    //HttpMessageNotReadableException     когда в теле ничего не передано
+
+    @ExceptionHandler(NoResourceFoundException.class)
+    public ResponseEntity<List<ErrorResponse>> onNoHandlerFoundException() {
+        ErrorResponse errorResponse = new ErrorResponse(EError.ERROR,
+                "Введен неверный URL");
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(List.of(errorResponse));
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<List<ErrorResponse>> onException(Exception exception) {
+        String message = exception.getMessage();
+        String name = exception.getClass().getName();
+        ErrorResponse errorResponse = new ErrorResponse(EError.ERROR,
+                "Сервер не смог корректно обработать запрос. Попробуйте позже или обратитесь к администратору");
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(List.of(errorResponse));
+    }
+
+
+
+
+
+}
