65fe78b665809f36eac5175942e1e3b4483fb170
Peniaz Evgenii <evgenii.peniaz@gmail.com>
2024-11-24 21:27:22 +0300

    Solution N+1 problem with EntityGraph

--- audit-service/src/main/java/by/it_academy/jd2/auditservice/repository/IAuditRepository.java
+++ audit-service/src/main/java/by/it_academy/jd2/auditservice/repository/IAuditRepository.java
@@ -1,10 +1,15 @@
 package by.it_academy.jd2.auditservice.repository;
 
 import by.it_academy.jd2.auditservice.repository.entity.AuditEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.UUID;
 
 public interface IAuditRepository extends JpaRepository<AuditEntity, UUID> {
 
+    @EntityGraph(attributePaths = "user")
+    Page<AuditEntity> findAllBy(Pageable pageable);         //decide проблем N + 1
 }
